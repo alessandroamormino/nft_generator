37dad61343ad9436d3b500f87e22b21970a93f28
Alessandro Amormino <amormino.alessandro97@gmail.com>
2022-01-08 17:23:33 +0100

    add eyes to the draw canvas

--- script.js
+++ script.js
@@ -9,15 +9,27 @@ window.onload = function() {
         buildrobot();
     }
 
+    //eyes
+    var roboteye = new Image();
+    var roboteyenum = Math.floor(Math.random() * 2) + 1; // 2 num of images i have, change for more heads
+    var roboteyename = "eye" + roboteyenum + ".png";
+    roboteye.src = roboteyename;
+
+    roboteye.onload = function() {
+        buildrobot();
+    }
+
     function buildrobot() {
         //create a canvas for drawing the image
         let canvas = document.getElementById('canvas');
         let ctx = canvas.getContext('2d');
-        canvas.width = 400;
-        canvas.height = 400;
+        canvas.width = 500;
+        canvas.height = 500;
 
         //draw head
-        ctx.drawImage(robothead, ((400 - robothead.width) / 2), 0);
+        ctx.drawImage(robothead, ((500 - robothead.width) / 2), 50);
+        //draw eyes
+        ctx.drawImage(roboteye, ((500 - roboteye.width) / 2), 25);
 
     }
 }
